d3a06d64bcb01222d50352d13b77c2cda8e70dd4
Javier <javiferreyra97@gmail.com>
2022-04-15 20:45:01 -0300

    Add getEvents query

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,11 +1,15 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query';
 import {getEnvironment} from '../config/environment';
+import {EventModel} from '../model/Event';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({baseUrl: getEnvironment().apiHost}),
   endpoints: (builder) => ({
+    getEvents: builder.query<EventModel, {page: number, pageSize: number}>({
+      query: (params) => `/e/events?Start=${params.page}&PageSize=${params.pageSize}`,
+    })
   })
 })
 
 
-export const {} = api;
+export const {useGetEventsQuery} = api;
