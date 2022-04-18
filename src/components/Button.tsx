a07294c3f426b9f188a0749301a558b5e9f3dad2
Javier <javiferreyra97@gmail.com>
2022-04-18 16:32:51 -0300

    Implement custom button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import {ButtonProps, Text, View, Button as DefaultButton} from 'react-native';
+import {ButtonProps, TouchableOpacity, StyleSheet, StyleProp, ViewStyle, TextStyle} from 'react-native';
 import {colors} from '../styles/colors';
+import { squares } from '../styles/grid';
+import {H3} from './Typography';
 
-const Button = (props: ButtonProps) => {
+interface Props extends ButtonProps {
+  style: StyleProp<ViewStyle>,
+  textStyle: StyleProp<TextStyle>
+}
+
+const Button = (props: Props) => {
   return (
-    <DefaultButton color={colors.primary} {...props}/>
+    <TouchableOpacity {...props} style={[styles.button, props.style]}>
+      <H3 style={[styles.text, props.textStyle]}>{props.title}</H3>
+    </TouchableOpacity>
   );
 };
 
 export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: squares(2),
+  },
+  text: {
+    color: colors.white,
+    margin: squares(2)
+  }
+});
