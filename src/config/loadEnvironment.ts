66238e43c963114df682d79773c9564def987e24
Javier <javiferreyra97@gmail.com>
2022-11-28 13:05:12 -0300

    Show env variables in dev mode + throw errors if not defined

--- src/config/loadEnvironment.ts
+++ src/config/loadEnvironment.ts
@@ -1,9 +1,10 @@
 import {TEST_API_SECRET, KEYCLOAK_CLIENT_ID, KEYCLOAK_CLIENT_SECRET} from 'react-native-dotenv'
 
 export const env = (() => {
-
   const API_SECRET = process.env.API_SECRET || TEST_API_SECRET
   if (!API_SECRET) throw new Error('API_SECRET and TEST_API_SECRET not defined')
+  if (!KEYCLOAK_CLIENT_ID) throw new Error('KEYCLOAK_CLIENT_ID not defined')
+  if (!KEYCLOAK_CLIENT_SECRET) throw new Error('KEYCLOAK_CLIENT_SECRET not defined')
 
   return {
     API_SECRET,
@@ -11,3 +12,5 @@ export const env = (() => {
     KEYCLOAK_CLIENT_ID,
   }
 })()
+
+__DEV__ && console.log('env', env)
