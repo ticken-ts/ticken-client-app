d07b54e3400cd2d832bffdd13ee5c5fd7f64e185
Javier <javiferreyra97@gmail.com>
2023-03-08 22:53:13 -0300

    Improbe WIP user profile UI

--- src/screens/UserProfile.tsx
+++ src/screens/UserProfile.tsx
@@ -6,38 +6,51 @@ import {colors} from '@app/styles/colors';
 import FocusAwareStatusBar from '@app/components/FocusAwareStatusBar';
 import Button from '@app/components/Button';
 import {t} from '@app/locale/useLocalization';
-import useAppDispatch from '@app/hooks/useDispatch';
-import Typography from '@app/components/Typography';
+import Typography, {H1, H2} from '@app/components/Typography';
 import {ScreenProps} from '@app/navigation/mainStack/types';
 import {ScreenId} from '@app/navigation/mainStack/ScreenIDs';
 import {AuthContext} from '@app/context/AuthContext';
+import {squares} from '@app/styles/grid';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Logo from '@app/assets/logo.svg';
 
 const UserProfile = ({navigation}: ScreenProps<ScreenId.UserProfile>) => {
 
-  const dispatch = useAppDispatch()
-
   const {logout, login, ready, token, isLoggedIn} = useContext(AuthContext);
+
+  const insets = useSafeAreaInsets()
 
   if (isLoggedIn) {
     return (
       <View style={styles.container}>
-        <Typography>You are logged in</Typography>
-        <Typography>{token?.slice(0, 10)}...</Typography>
+        <FocusAwareStatusBar style={'dark'} />
+        <View style={styles.main}>
+          <Logo width={squares(25)} height={squares(25)} />
+          <H1>Coming soon</H1>
+        </View>
         <Button
           onPress={logout}
           title={t('logOut')}
+          style={styles.signInButton}
         />
+        <View style={{height: insets.bottom || squares(2)}} />
       </View>
     )
   } else {
     return (
       <View style={styles.container}>
         <FocusAwareStatusBar style={'dark'} />
-        <Typography>You are not logged in</Typography>
+        <View style={styles.main}>
+          <Logo width={squares(25)} height={squares(25)} />
+          <H1>You are not logged in</H1>
+          <Typography>Sign In or create an account and start buying tickets!</Typography>
+        </View>
         <Button
           onPress={login}
           title={t('login')}
+          style={styles.signInButton}
         />
+        <View style={{height: insets.bottom || squares(2)}} />
       </View>
     );
   }
@@ -48,6 +61,7 @@ export default {
   component: UserProfile,
   options: getTranslucentHeader({
     left: () => <BackButton />,
+    mid: () => <H2 style={{color: colors.primary}}>User Profile</H2>,
     backgroundColor: colors.primary,
   })
 };
@@ -55,7 +69,15 @@ export default {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center'
+  },
+  main: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
   },
+  signInButton: {
+    margin: squares(2)
+  }
 });
