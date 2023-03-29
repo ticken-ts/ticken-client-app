7f722ac98fa3e0f48ec626bf3897ece6b09f552b
Javier <javiferreyra97@gmail.com>
2023-03-29 12:51:36 -0300

    fix useToggle return type

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 
-export const useToggle = () => {
+export const useToggle = (): [boolean, () => void] => {
   const [value, setValue] = useState(false);
   const toggle = () => setValue((v) => !v);
   return [value, toggle];
