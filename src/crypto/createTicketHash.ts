f7e879e65f8de2a0b01aa98de7ef54b5b7b02d1f
Javier <javiferreyra97@gmail.com>
2023-03-29 19:10:33 -0300

    Add dev console logs and some small fixes

--- src/crypto/createTicketHash.ts
+++ src/crypto/createTicketHash.ts
@@ -1,7 +1,9 @@
 import { ApiEvent, ApiTicket } from "@app/api/models";
 import { env } from "@app/config/loadEnvironment";
 import totp from "totp-generator";
-import crypto, { CryptoDigestAlgorithm } from "expo-crypto";
+import * as crypto from "expo-crypto";
+import { RSA } from 'react-native-rsa-native';
+import { Buffer } from "buffer";
 
 const totpSecret = env.TOTP_SECRET
 
@@ -10,26 +12,35 @@ export const createTicketHash = async (ticket: ApiTicket, event: ApiEvent, priva
     
     const fingerprint = await getTicketFingerprint(ticket, event)
 
+    __DEV__ && console.log("Fingerprint: ", fingerprint)
+
     const hash = encodeURIComponent(fingerprint)
 
-    // TODO: encrypt using RSA PSS with SHA-256 and MGF1 (SHA-256)
+    const encrypted = await RSA.signWithAlgorithm(hash, privateKey, "SHA256withRSA")
     
-    return hash
+    __DEV__ && console.log("Encrypted hash: ", encrypted)
+
+    return encrypted
 }
 
 const getTicketFingerprint = async (ticket: ApiTicket, event: ApiEvent) => {
+
+    __DEV__ && console.log("Generating TOTP token using:", totpSecret)
+
     const totpToken = totp(totpSecret, {
         digits: 8,
         algorithm: "SHA-512",
         period: 10,
     })
     
+    __DEV__ && console.log("Token ID: ", ticket.token_id)
+
     // Convert to hex using Buffer
     const tokenIDHex = Buffer.from(ticket.token_id).toString("hex")
 
     const fingerprint = `${event.pub_bc_address}/${tokenIDHex}/${totpToken}`
 
-    const hashSha256 = await crypto.digestStringAsync(CryptoDigestAlgorithm.SHA256, fingerprint)
+    const hashSha256 = await crypto.digestStringAsync(crypto.CryptoDigestAlgorithm.SHA256, fingerprint)
 
     return hashSha256
 }
