b3f450bdc394d89bd541761b763c8a10e4d6bb85
facutorraca <ftorraca@fi.uba.ar>
2023-04-18 01:37:18 -0300

    fix ticket signature generation

--- src/crypto/createTicketHash.ts
+++ src/crypto/createTicketHash.ts
@@ -6,66 +6,63 @@ import { Buffer } from "buffer";
 import { ec } from "elliptic";
 import { DateTime } from "luxon";
 
-const totpSecret = env.TOTP_SECRET
+const totpSecret = env.TOTP_SECRET;
 
 export const createTicketHash = async (ticket: ApiTicket, event: ApiEvent, privateKey: string) => {
-    
-    const {fingerprintHash, expirationDateSeconds} = await getTicketFingerprint(ticket, event)
+  const { fingerprintHash, expirationDateSeconds } = await getTicketFingerprint(ticket, event);
 
-    __DEV__ && console.log("Fingerprint: ", fingerprintHash)
+  __DEV__ && console.log("Fingerprint: ", fingerprintHash);
 
-    const uriEncodedFingerprint = encodeURIComponent(fingerprintHash)
+  __DEV__ && console.log("Private key: ", privateKey);
 
-    __DEV__ && console.log("Encoded fingerprint: ", uriEncodedFingerprint)
+  const encrypted = await (async () => {
+    const curve = new ec("secp256k1");
+    const key = curve.keyFromPrivate(privateKey, "hex");
+    const encrypted = key.sign(fingerprintHash, "hex");
+    return encrypted;
+  })();
 
-    __DEV__ && console.log("Private key: ", privateKey)
+  const signature = JSON.stringify({
+    r: encrypted.r.toString("hex"),
+    s: encrypted.s.toString("hex"),
+    eventID: event.event_id,
+    ticketID: ticket.ticket_id,
+  });
 
-    const encrypted = await (async () => {
-        const curve = new ec("secp256k1")
-        const key = curve.keyFromPrivate(privateKey, "hex")
-        const encrypted = key.sign(uriEncodedFingerprint, "hex")
-        return encrypted
-    })()
+  __DEV__ && console.log("Encrypted hash: ", signature);
 
-    const signature = JSON.stringify({
-        r: encrypted.r.toString("hex"),
-        s: encrypted.s.toString("hex"),
-        eventID: event.event_id,
-        ticketID: ticket.ticket_id,
-    })
-
-    __DEV__ && console.log("Encrypted hash: ", signature)
-
-    return {signature, expirationDateSeconds}
-}
+  return { signature, expirationDateSeconds };
+};
 
 // Returns a SHA256 hash of the ticket fingerprint (event address + token ID + TOTP token)
 const getTicketFingerprint = async (ticket: ApiTicket, event: ApiEvent) => {
+  __DEV__ && console.log("Generating TOTP token using:", totpSecret);
 
-    __DEV__ && console.log("Generating TOTP token using:", totpSecret)
-    
-    const periodSeconds = 60
-    const expirationDateSeconds = Math.floor(DateTime.now().toUnixInteger() / periodSeconds) * 60 + periodSeconds
+  const periodSeconds = 60 * 30; // 5 mins;
+  const expirationDateSeconds =
+    Math.floor(DateTime.now().toUnixInteger() / periodSeconds) * periodSeconds + periodSeconds;
 
-    __DEV__ && console.log("Expiration date: ", expirationDateSeconds)
+  __DEV__ && console.log("Expiration date: ", expirationDateSeconds);
 
-    const totpToken = await (async () => totp(totpSecret, {
-        digits: 8,
-        algorithm: "SHA-512",
-        period: periodSeconds,
-        timestamp: Date.now(),
-    }))()
+  const totpToken = await (async () =>
+    totp(totpSecret, {
+      digits: 8,
+      algorithm: "SHA-512",
+      period: periodSeconds,
+      timestamp: Date.now(),
+    }))();
 
-    __DEV__ && console.log("TOTP token: ", totpToken)
-    
-    __DEV__ && console.log("Token ID: ", ticket.token_id)
+  __DEV__ && console.log("TOTP token: ", totpToken);
 
-    // Convert to hex using Buffer
-    const tokenIDHex = Buffer.from(ticket.token_id).toString("hex")
+  __DEV__ && console.log("Token ID: ", ticket.token_id);
 
-    const fingerprint = `${event.pub_bc_address}/${tokenIDHex}/${totpToken}`
+  // Convert to hex using Buffer
+  const fingerprint = `${event.pub_bc_address}/${ticket.token_id}/${totpToken}`;
 
-    const fingerprintHash = await crypto.digestStringAsync(crypto.CryptoDigestAlgorithm.SHA256, fingerprint)
+  const fingerprintHash = await crypto.digestStringAsync(
+    crypto.CryptoDigestAlgorithm.SHA256,
+    fingerprint
+  );
 
-    return {fingerprintHash, expirationDateSeconds}
-}
+  return { fingerprintHash, expirationDateSeconds };
+};
