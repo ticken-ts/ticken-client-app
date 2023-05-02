4c8b1beed6a26858507b771b89c62184fdb72e21
facundotorraca <ftorraca@fi.uba.ar>
2023-05-02 02:43:24 -0300

    add padding in events screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,54 +1,57 @@
-import React, {useEffect, useState} from 'react';
-import {FlatList, RefreshControl, StyleSheet, View} from 'react-native';
-import {ScreenProps} from '@app/navigation/mainStack/types';
-import {ScreenId} from '@app/navigation/mainStack/ScreenIDs';
-import {getCustomHeader} from '@app/navigation/mainStack/headers';
-import {colors} from '@app/styles/colors';
-import {H1} from '@app/components/Typography';
-import {HomeListItem} from '@app/components/HomeListItem';
-import FocusAwareStatusBar from '@app/components/FocusAwareStatusBar';
-import {t} from '@app/locale/useLocalization';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import {squares} from '@app/styles/grid';
-import {useEventListQuery} from '@app/api/useEventListQuery';
+import React, { useEffect, useState } from "react";
+import { FlatList, RefreshControl, StyleSheet, View } from "react-native";
+import { ScreenProps } from "@app/navigation/mainStack/types";
+import { ScreenId } from "@app/navigation/mainStack/ScreenIDs";
+import { getCustomHeader } from "@app/navigation/mainStack/headers";
+import { colors } from "@app/styles/colors";
+import { H1 } from "@app/components/Typography";
+import { HomeListItem } from "@app/components/HomeListItem";
+import FocusAwareStatusBar from "@app/components/FocusAwareStatusBar";
+import { t } from "@app/locale/useLocalization";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { squares } from "@app/styles/grid";
+import { useEventListQuery } from "@app/api/useEventListQuery";
 
 const pageSize = 4;
 
-const Home = ({navigation}: ScreenProps<ScreenId.Home>) => {
-
+const Home = ({ navigation }: ScreenProps<ScreenId.Home>) => {
   const goToProfile = () => {
-    navigation.navigate(ScreenId.UserProfile, {})
+    navigation.navigate(ScreenId.UserProfile, {});
   };
 
   useEffect(
-    () => navigation.setOptions(getCustomHeader({
-      mid: () => <H1 style={{color: colors.white}}>{t('homeTitle')}</H1>,
-      right: () => (
-        <Ionicons
-          name={'person-circle'}
-          color={colors.white}
-          style={styles.profileIcon}
-          onPress={goToProfile}
-          size={squares(3)}
-        />
+    () =>
+      navigation.setOptions(
+        getCustomHeader({
+          mid: () => <H1 style={{ color: colors.white }}>{t("homeTitle")}</H1>,
+          right: () => (
+            <Ionicons
+              name={"person-circle"}
+              color={colors.white}
+              style={styles.profileIcon}
+              onPress={goToProfile}
+              size={squares(3)}
+            />
+          ),
+          backgroundColor: colors.primary,
+        })
       ),
-      backgroundColor: colors.primary
-    })),
     []
-  )
+  );
 
-  const {isLoading, data, isFetching, refetch} = useEventListQuery();
+  const { isLoading, data, isFetching, refetch } = useEventListQuery();
 
   return (
     <View style={styles.container}>
-      <FocusAwareStatusBar translucent style={'light'} />
+      <FocusAwareStatusBar translucent style={"light"} />
       <FlatList
         style={styles.scroll}
-        refreshControl={<RefreshControl refreshing={isLoading || isFetching} onRefresh={refetch} /> }
+        refreshControl={<RefreshControl refreshing={isLoading || isFetching} onRefresh={refetch} />}
         data={data}
+        contentContainerStyle={{ paddingBottom: squares(2) }}
         onEndReachedThreshold={0.2}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({item}) => <HomeListItem item={item} />}
+        renderItem={({ item }) => <HomeListItem item={item} />}
       />
     </View>
   );
@@ -67,7 +70,6 @@ const styles = StyleSheet.create({
     // backgroundColor: colors.secondary
   },
   profileIcon: {
-    marginRight: squares(1)
-  }
+    marginRight: squares(1),
+  },
 });
-
